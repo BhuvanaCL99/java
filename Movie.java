5cd14c9c06d6bd6e2f2780af79e5c97e020fc870
BhuvanaCL99 <bhuvanacl.xworkz@gmail.com>
2021-09-07 23:52:54 +0530

    files added

---
+++ Movie.java
@@ -0,0 +1,15 @@
+class Movie {
+
+static String movieName="Maanikya";
+static String actorName="Kichha Sudeep";
+static String musicDirector="Arjun Janya";
+
+public static void main(String a[]){
+
+System.out.println( movieName);
+System.out.println( actorName);
+System.out.println( movieName);
+}
+}
+
+
