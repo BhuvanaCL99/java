34f241c8a092e86f64288e7c9a918c0b70c6a5be
BhuvanaCL99 <bhuvanacl.xworkz@gmail.com>
2021-09-07 22:15:33 +0530

    files added

---
+++ Hospital.java
@@ -0,0 +1,15 @@
+class Hospital{
+
+     public static void main(String a[]){
+
+            String hospitalName="Sri Devi Hospital";
+            String doctorName = "Dr. Santhosh";
+            String availableTime = "10:00 am";
+   
+   System.out.println(hospitalName);
+   System.out.println(doctorName);
+   System.out.println(availableTime);
+}
+}
+
+            
---
+++ Restaurant.java
@@ -0,0 +1,17 @@
+class Restaurant{
+ 
+    public static void main(String a[]){
+
+        int numberOfRestaurantInBangalore=500;
+        String popularRestaurantInRajajinagar="New Udupi Swada";
+        String timings="10:00am to 10:00pm";
+
+System.out.println(numberOfRestaurantInBangalore);
+System.out.println( popularRestaurantInRajajinagar);
+System.out.println(timings);
+}
+}
+
+
+
+     
